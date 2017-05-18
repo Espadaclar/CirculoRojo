1ee06b7466a2898eadef7d2223d11bfe51ae2c95
Espadaclar <eneroborde8002@gmail.com>
2017-05-18 23:39:31 +0200

    codigo para desplazar el círculo horizontalmente.

--- Circulo.java
+++ Circulo.java
@@ -1,18 +1,23 @@
-import javafx.stage.Stage;             // |
-import javafx.scene.Group;             // |\ Librerías necesarias
-import javafx.scene.Scene;             // |/ Para el ejemplo
-import javafx.application.Application; // |
-import javafx.scene.control.Button;    // |
-
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
-import javafx.scene.shape.Shape;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
+import javafx.scene.paint.*;
+
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.util.Duration;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.Button;
+
+// import javafx.animation.TranslateTransition;
+// import javafx.util.Duration;
+// import javafx.event.EventHandler;
+// import javafx.event.ActionEvent;
 /**
  * Write a description of class Circulo here.
  * 
@@ -30,20 +35,35 @@ public class Circulo extends Application
 
     public void start(Stage ventana){//parámetro que va ha ser la ventan de la aplicación
         Group root = new Group(); //contenedor que colocamos dentro de la escena.
-        Scene escena = new Scene(root, 500, 500);//Se crea la escena con el contenedor que contiene los objetos.
+        Scene escena = new Scene(root, 500, 500, Color.BLUE);//Se crea la escena con el contenedor que contiene los objetos.
         ventana.setScene(escena);//pasamos al parámetro ventana el objeto escena.
-        
+
         //Se crea el círculo
         Circle circle = new Circle();
         circle.setCenterX(250.0f);
         circle.setCenterY(250.0f);
         circle.setFill(Color.RED);
         circle.setRadius(20.0f);
-        
+
         //coloca el círculo dentro del contenedor root.
         root.getChildren().add(circle);
-        
+
+        //////////////////////////////PARA DESPLAZAR EL CÍRCULO.
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.setAutoReverse(true);
+        KeyFrame kf = new KeyFrame(Duration.seconds(.01),
+                new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) {
+                        circle.setTranslateX(circle.getTranslateX() + 1);
+                    }
+                });
+        timeline.getKeyFrames().
+        add(kf);
+        timeline.play();
+        /////////////////////////////////////////////////
 
         ventana.show();
     }
+
 }
